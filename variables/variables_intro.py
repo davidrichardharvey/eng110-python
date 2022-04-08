3360b611601d7ef19a1e9058f5af2577bd1b1c32
David Harvey <davidrharvey@hotmail.co.uk>
2022-04-08 11:29:12 +0100

    Looked at numeric types

---
+++ variables/variables_intro.py
@@ -0,0 +1,30 @@
+# a = 1  # int --> integer
+# b = 2  # int
+# c = 3.5  # float --> floating-point number
+# d = "hello"  # str --> string of characters
+#
+# # Dynamically Typed Language
+#
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+# ctrl + / = comment/uncomment
+
+# Numeric Types
+
+a = 125  # int
+b = 1.25  # float
+c = 3 + 4j  # complex
+
+print(c, type(c))
+
+# Play with these numbers
+# Try out the arithmetic operators: + - * / (bonus: //) %
+# What data type results from combining ints and/or floats with each operator?
+
+a = 1 / 3
+b = a * 3
+print(a, type(a))
+print(b, type(b))
